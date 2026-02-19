0405f502b56276dacf71270e3c96f92a46b1118d
Socorro DominguezVidana <seiryu88@icloud.com>
2026-02-19 10:36:26 -0800

    BU-349 improving dataset level

--- src/DataBUS/neotomaValidator/valid_geochron_dataset.py
+++ src/DataBUS/neotomaValidator/valid_geochron_dataset.py
@@ -6,10 +6,6 @@ def valid_geochron_dataset(cur, yml_dict, csv_file):
     Creates and validates a Dataset object with the geochronological dataset type ID.
     Used specifically for geochronological data that requires special dataset type handling.
 
-    Examples:
-        >>> valid_geochron_dataset(cursor, config_dict, "geochron_data.csv")
-        Response(valid=[True], message=[...], validAll=True)
-
     Args:
         cur (cursor): Database cursor to execute SQL queries.
         yml_dict (dict): Dictionary containing YAML configuration data.
@@ -18,21 +14,19 @@ def valid_geochron_dataset(cur, yml_dict, csv_file):
     Returns:
         Response: Response object containing validation messages and overall validity status.
 
-    Raises:
-        Exception: If there are issues retrieving values from YAML dict or creating the dataset.
+    Examples:
+        >>> valid_geochron_dataset(cursor, config_dict, "geochron_data.csv")
+        Response(valid=[True], message=[...], validAll=True)
     """
     response = Response()
-
-    inputs = {}
-    inputs['datasettypeid'] = 1
-
+    inputs = {'datasettypeid': 1,
+              'collectionunitid': 1} #CU is a placeholder value to allow for creation
     try:
         Dataset(**inputs)
-        response.message.append(f"✔ Geochronology Dataset can be created.")
+        response.message.append("✔ Geochronology Dataset can be created.")
         response.valid.append(True)
     except Exception as e:
         response.message.append(f"✗ Geochronology Dataset cannot be created: {e}")
         response.valid.append(False)
-
-    response.message = list(set(response.message))
+    
     return response
